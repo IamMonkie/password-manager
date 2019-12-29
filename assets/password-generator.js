b66224a8c130e745debb9613acd02f0ed09441ee
IamMonkie <monkie_225@yahoo.com>
2019-12-29 01:24:58 -0500

    added event listeners  for checkboxes

--- assets/password-generator.js
+++ assets/password-generator.js
@@ -1,10 +1,32 @@
 // variables
 const charLimitRange = document.getElementById("charLimitRange");
 const charLimitNumber = document.getElementById("charLimitNumber");
+const form = document.getElementById("passwordGeneratorForm");
+const upperCaseElement = document.getElementById("includeUpperCase");
+const symbolsElement = document.getElementById("includeSymbols");
+const numbersElement = document.getElementById("includeNumbers");
 
 // event listeners
 charLimitNumber.addEventListener("input", syncCharacterAmount);
 charLimitRange.addEventListener("input", syncCharacterAmount);
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const syncCharacterLimit = charLimitNumber.value;
+
+  // checkboxes
+  const upperCaseCheck = upperCaseElement.checked;
+
+  const symbolsCheck = symbolsElement.checked;
+
+  const numbersCheck = numbersElement.checked;
+
+  const password = generatePassword(
+    syncCharacterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+});
 
 // functions
 function syncCharacterAmount(e) {
